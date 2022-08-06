ae7dc5759a0ddb32106731ad306767ebbebfcb78
jay <dj.min43@gmail.com>
2022-08-06 16:33:05 +0900

    clean

--- packages/ui/Button.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-interface ButtonProps {
-  buttonName: string;
-}
-
-export const Button = ({ buttonName }: ButtonProps) => {
-  return <button>{buttonName}</button>;
-};
